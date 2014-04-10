c7569672fb475c2f043fc54220785b48544f891b
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2014-04-10 11:31:33 +0200

    working timer

--- assets/script.js
+++ assets/script.js
@@ -14,10 +14,34 @@ function drawStar(x, y) {
     ctx.fillRect(x,y,1,1);
 }
 
-clear();
-for (var i = 0; i < 1000; ++i) {
-    drawStar(Math.round(Math.random() * 600), Math.round(Math.random() * 400));
+function drawStars() {
+    clear();
+    for (var i = 0; i < 1000; ++i) {
+        drawStar(Math.round(Math.random() * 600), Math.round(Math.random() * 400));
+    }
 }
 
+drawStars();
 
+function updateTimer() {
+  var finalDate = new Date('18 December 2015');
+  var difference = (finalDate.getTime() - (new Date()).getTime()) / 1000;
+  var daysDom = document.getElementById('days');
+  var hoursDom = document.getElementById('hours');
+  var minutesDom = document.getElementById('minutes');
+  var secondsDom = document.getElementById('seconds');
+  var days = Math.floor(difference / (60 * 60 * 24));
+  difference -= days * 60 * 60 * 24;
+  var hours = Math.floor(difference / (60 * 60));
+  difference -= hours * 60 * 60;
+  var minutes = Math.floor(difference / (60));
+  difference -= minutes * 60;
+  daysDom.innerHTML = days;
+  hoursDom.innerHTML = hours;
+  minutesDom.innerHTML = minutes;
+  secondsDom.innerHTML = Math.floor(difference);
+}
+
+updateTimer();
+setInterval(updateTimer, 1000);
 
